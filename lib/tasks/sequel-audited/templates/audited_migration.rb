36ebca6e9e95fcb26b0f8c025d566185e3f0782d
Arkanain <arkanainKiller@gmail.com>
2016-08-23 16:34:45 +0300

    update table name in template

--- lib/tasks/sequel-audited/templates/audited_migration.rb
+++ lib/tasks/sequel-audited/templates/audited_migration.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   change do
-    create_table(:audited_logs) do
+    create_table :audit_logs do
       primary_key :id
       column :model,            String
       column :model_pk,         String
@@ -11,33 +11,7 @@ Sequel.migration do
       column :username,         String
       column :user_type,        String
       column :created_at,       :timestamp
-      
-      
-      # column :audited_id,       :integer
-      # column :audited_type,     :string
-      # column :associated_id,    :integer
-      # column :associated_type,  :string
-      # column :action,           :string
-      
-      # column :auditable_id,     :integer
-      # column :auditable_type,   :string
-      # column :associated_id,    :integer
-      # column :associated_type,  :string
-      # column :user_id,          :integer
-      # column :user_type,        :string
-      # column :username,         :string
-      # column :action,           :string
-      # column :audited_changes,  :text
-      # column :version,          :integer, :default => 0
-      # column :comment,          :string
-      # column :remote_address,   :string
-      # column :request_uuid,     :string
-      # column :created_at,       :datetime
-      #
-      # add_index :audits, [:auditable_id, :auditable_type], :name => 'auditable_index'
-      # add_index :audits, [:associated_id, :associated_type], :name => 'associated_index'
-      # add_index :audits, [:user_id, :user_type], :name => 'user_index'
-      # add_index :audits, :request_uuid
+
       index :created_at
     end
   end
